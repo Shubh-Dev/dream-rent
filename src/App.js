da0c5e75fbb2f9ee2ca38d0cf180080245ad0e0a
Shubh <shubh.scb@gmail.com>
2022-11-28 18:32:16 +0530

    SIDEBAR COMPLETE

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ import ReserveList from './components/ReserveList';
 import MyReservations from './components/MyReservations';
 import Sidebar from './components/Sidebar';
 import DeleteHouse from './components/DeleteHouse';
-import Hamburger from './components/Hamburger';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="sidebar-container">
-        <Hamburger />
         <Sidebar />
       </div>
 
--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,10 +1,11 @@
-import React from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Hamburger = () => (
+
   <div className="hamburger">
     <GiHamburgerMenu />
   </div>
+
 );
 
 export default Hamburger;
