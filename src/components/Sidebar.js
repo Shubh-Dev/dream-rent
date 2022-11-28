78fa77fe878352a72f35fc56c7e15a9519ac329d
Shubh <shubh.scb@gmail.com>
2022-11-28 22:27:59 +0530

    ?

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,49 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import SocialIcon from './SocialIcon';
 import Hamburger from './Hamburger';
 import './sidebar.css';
 
-const Sidebar = () => (
-  <div className="nav-bar-container">
+const Sidebar = () => {
+  const [openSidebar, setSidebar] = useState(true);
 
-    <div className="hamburger-container">
-      <Hamburger />
-    </div>
+  const toggleSidebar = () => {
+    console.log('toggleSidebar');
+    setSidebar(!openSidebar);
+  };
 
-    <div className="logo-container">
-      <h4>My-logo</h4>
-    </div>
+  return (
+    <div className="nav-bar-container">
 
-    <div className="nav-links-container">
-      <NavLink to="/">
-        HouseList
-      </NavLink>
-
-      <NavLink to="/add-house">
-        Add House
-      </NavLink>
-
-      <NavLink to="/reserve-list">
-        Reserve List
-      </NavLink>
-
-      <NavLink to="/my-reservations">
-        My Reservations
-      </NavLink>
-
-      <NavLink to="/delete-house">
-        Delete House
-      </NavLink>
-
-      <div className="social-logos">
-        <SocialIcon />
-        <p>&copy; 2022 Dream rent & Co</p>
+      <div className="hamburger-container">
+        <Hamburger onClick={toggleSidebar} />
       </div>
+
+      <div className={`logo-container ${openSidebar ? 'show' : 'hide'}`}>
+        <h4>My-logo</h4>
+      </div>
+
+      <div className={`nav-links-container ${openSidebar ? 'show' : 'hide'}`}>
+        <NavLink to="/">
+          HouseList
+        </NavLink>
+
+        <NavLink to="/add-house">
+          Add House
+        </NavLink>
+
+        <NavLink to="/reserve-list">
+          Reserve List
+        </NavLink>
+
+        <NavLink to="/my-reservations">
+          My Reservations
+        </NavLink>
+
+        <NavLink to="/delete-house">
+          Delete House
+        </NavLink>
+
+        <div className="social-logos">
+          <SocialIcon />
+          <p>&copy; 2022 Dream rent & Co</p>
+        </div>
+      </div>
+
     </div>
-
-  </div>
-
-);
+  );
+};
 
 export default Sidebar;
