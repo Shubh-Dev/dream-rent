ddd3259b3276b5112f8c7ddf06c0b2817c7ad072
tugsem <saranlitugsem@gmail.com>
2022-11-30 16:41:28 +0300

    Finish styling of mobile list

--- src/components/HouseList.js
+++ src/components/HouseList.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import {
+  FaFacebookF, FaTwitter, FaInstagram,
+} from 'react-icons/fa';
 import { fetchHouses } from '../redux/house/houses';
 
 const HouseList = () => {
@@ -10,15 +13,32 @@ const HouseList = () => {
   }, []);
   const houses = useSelector((state) => state.houses);
   return (
-    <div className="mainList d-flex flex-column container">
-      {houses.map((house) => (
-        <div className="card" key={house.id}>
-          <Link to={`houses/${house.id}`}>
-            <img style={{ width: '50%', height: '70%' }} src={house.image_path} alt="house_image" />
-          </Link>
-
-        </div>
-      ))}
+    <div className="container text-center pt-5">
+      <h1>LATEST HOUSES</h1>
+      <h2 className="lgrey-text">Please select a house model</h2>
+      <div className="main-list d-flex flex-column container pt-5">
+        {houses.map((house) => (
+          <div className="house-container mb-3 text-center" key={house.id}>
+            <Link to={`houses/${house.id}`}>
+              <img className="homepage-house-img " style={{ width: '100%', height: '100%' }} src={house.image_path} alt="house_image" />
+            </Link>
+            <h3 className="mt-3">
+              {house.house_type.toUpperCase()}
+              {' '}
+              HOUSE
+            </h3>
+            <div className="mt-3 d-flex justify-content-center">
+              <div className="bottom-border" />
+            </div>
+            <h4 className="mt-3 lgrey-text">{house.address}</h4>
+            <div className="house-icons d-flex justify-content-center gap-3 mt-5">
+              <FaFacebookF className="lgrey-text" size={30} />
+              <FaTwitter className="lgrey-text" size={30} />
+              <FaInstagram className="lgrey-text" size={30} />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
