9c81c498978df5d33557c47550293d4114db8324
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-11-30 18:35:22 +0500

    Updated styling of the page

--- src/components/HouseList.js
+++ src/components/HouseList.js
@@ -32,13 +32,13 @@ const HouseList = () => {
     return <div>Loading...</div>;
   }
   return (
-    <div className="card-group m-5" style={{ width: '25rem' }}>
+    <div className="card-group m-5" style={{ width: '40rem' }}>
       { houses.map((house) => (
         <div className="card d-flex align-items-center border border-0" key={house.id}>
           <img src={house.image_path} className="card-img-top img-fluid thumbnail h-50 w-50 rounded-circle" alt="house comes here" />
           <div className="card-body align-items-center">
             <Link to={`houses/${house.id}`}>
-              <h5 className="card-title fs-5">{house.address}</h5>
+              <h6 className="card-title fs-6">{house.address}</h6>
             </Link>
             <p className="card-text">{house.house_type}</p>
           </div>
