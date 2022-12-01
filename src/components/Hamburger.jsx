05e41eb3381602ec4f52086b54b739852de9828c
Shubh <shubh.scb@gmail.com>
2022-12-01 23:02:40 +0530

    ERR FIX

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -2,7 +2,6 @@ import { React, useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { AiOutlineClose } from 'react-icons/ai';
 import Sidebar from './Sidebar';
-import './hamburger.css';
 
 const Hamburger = () => {
   const sideBarStyle = {
