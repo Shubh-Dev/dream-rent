ae3b2fc0abd7872f977c835845c8af19b49db179
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-12-02 22:06:18 +0500

    Added function to save data on reserve page

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,10 +2,12 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import houseReducer from './house/houses';
 import userReducer from './users/users';
+import reserveReducer from './reserves/reserves';
 
 const rootReducer = combineReducers({
   houses: houseReducer,
   users: userReducer,
+  reserves: reserveReducer,
 });
 
 const store = configureStore({
--- src/webpages/reserves.js
+++ src/webpages/reserves.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 import { fetchHouses } from '../redux/house/houses';
 import { fetchUsers } from '../redux/users/users';
+import { addReserves } from '../redux/reserves/reserves';
 
 const Reserve = () => {
   const dispatch = useDispatch();
@@ -12,12 +14,23 @@ const Reserve = () => {
   const houses = useSelector((state) => state.houses);
   const users = useSelector((state) => state.users);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addReserves({
+      id: uuidv4(),
+      date: e.target.date.value,
+      time: e.target.time.value,
+      user_id: e.target.user_id.value,
+      house_id: e.target.house_id.value,
+    }));
+    e.target.reset();
+  };
   return (
-    <form className="col gx-3 gy-2 align-items-center m-5 p-5">
+    <form onSubmit={handleSubmit} className="col gx-3 gy-2 align-items-center m-5 p-5">
       <div className="row-sm-3 pb-3">
         <div className="input-group">
           <div className="input-group date" id="datepicker">
-            <input type="text" className="form-control" />
+            <input type="text" className="form-control" id="date" />
             <span className="input-group-append">
               <span className="input-group-text bg-white d-block">
                 <i className="fa fa-calendar" />
@@ -28,26 +41,26 @@ const Reserve = () => {
       </div>
       <div className="row-sm-3 pb-3">
         <div className="input-group">
-          <div className="input-group date" id="timepicker">
-            <input type="time" className="form-control" />
+          <div className="input-group date">
+            <input type="time" className="form-control" id="time" />
           </div>
         </div>
       </div>
       <div className="row-sm-3 pb-3">
-        <select className="form-select" id="specificSizeSelect">
+        <select className="form-select" id="house_id">
           <option selected>Choose House...</option>
           {houses.map((item) => (
-            <option key={item.id}>
+            <option key={item.id} value={item.id}>
               {item.address}
             </option>
           ))}
         </select>
       </div>
       <div className="row-sm-3 pb-3">
-        <select className="form-select" id="specificSizeSelect">
+        <select className="form-select" id="user_id">
           <option selected>Choose User...</option>
           {users.map((item) => (
-            <option key={item.id}>
+            <option key={item.id} value={item.id}>
               {item.name}
             </option>
           ))}
