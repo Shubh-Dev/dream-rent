fc20337d84c3a644978db4db2cfbdbbe9337b42d
Hamma Azarok <hammaagazarok@gmail.com>
2022-12-03 14:07:33 +0200

    fix linters

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -27,7 +27,7 @@ const Hamburger = () => {
     cursor: 'pointer',
     width: '50px',
     height: '50px',
-    padding: '10px'
+    padding: '10px',
   };
 
   const hamburgerStyle = {
@@ -36,7 +36,7 @@ const Hamburger = () => {
     fontSize: '1.5rem',
     width: '50px',
     height: '50px',
-    padding: '10px'
+    padding: '10px',
   };
 
   const [isOpen, setIsOpen] = useState(false);
