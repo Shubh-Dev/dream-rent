df7cc93d6c98d0e310f13d7d5797766308ab0acd
Shubh <shubh.scb@gmail.com>
2022-12-06 18:46:14 +0530

    BUILD

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   // <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   // </React.StrictMode>
 );
