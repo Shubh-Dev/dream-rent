f2d9919a77a7f2bd30c6a8cdca9272f10e975ea1
Shubh <shubh.scb@gmail.com>
2022-12-07 14:50:44 +0530

    ALL TESTS PASS

--- src/__tests__/MyReservations.test.js
+++ src/__tests__/MyReservations.test.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import TestRenderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
 import MyReservations from '../components/MyReservations';
 import store from '../redux/configureStore';
-import { Provider } from 'react-redux';
 
 describe('MyReservations', () => {
-    it('renders correctly', () => {
-        const tree = TestRenderer.create(
-        <Provider store={store}>
-            <MyReservations />
-        </Provider>,
-        ).toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+  it('renders correctly', () => {
+    const tree = TestRenderer.create(
+      <Provider store={store}>
+        <MyReservations />
+      </Provider>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-    it('display MY RESERVATIONS', () => {
-        const testRenderer = TestRenderer.create(
-        <Provider store={store}>
-            <MyReservations />
-        </Provider>,
-        );
-        const testInstance = testRenderer.root;
-        const h1 = testInstance.findByType('h1');
-    });
-    });
+  it('display MY RESERVATIONS', () => {
+    const testRenderer = TestRenderer.create(
+      <Provider store={store}>
+        <MyReservations />
+      </Provider>,
+    );
+    const testInstance = testRenderer.root;
+    const h1 = testInstance.findByType('h1');
+    expect(h1.children).toEqual(['My Reservations']);
+  });
+});
--- src/__tests__/ReservationForm.test.js
+++ src/__tests__/ReservationForm.test.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import TestRenderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
 import ReservationForm from '../components/ReservationForm';
 import store from '../redux/configureStore';
-import { Provider } from 'react-redux';
 
 describe('ReservationForm', () => {
-    it('renders correctly', () => {
-        const tree = TestRenderer.create(
-        <Provider store={store}>
-            <ReservationForm />
-        </Provider>,
-        ).toJSON();
-        expect(tree).toMatchSnapshot();
-    });
-
-    it('display RESERVATION FORM', () => {
-        const testRenderer = TestRenderer.create(
-        <Provider store={store}>
-            <ReservationForm />
-        </Provider>,
-        );
-        const testInstance = testRenderer.root;
-        const h1 = testInstance.findByType('h1');
-        });
-    });
+  it('renders correctly', () => {
+    const tree = TestRenderer.create(
+      <Provider store={store}>
+        <ReservationForm />
+      </Provider>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
