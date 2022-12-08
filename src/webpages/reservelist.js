e7917080617607de2a54ae4b9ba1c3ffa905ada6
Shubh <shubh.scb@gmail.com>
2022-12-08 22:09:10 +0530

    BUG FIX

--- src/webpages/reservelist.js
+++ src/webpages/reservelist.js
@@ -14,48 +14,51 @@ const ReserveList = () => {
   const reserves = useSelector((state) => state.reserves);
   const houses = useSelector((state) => state.houses);
   const users = useSelector((state) => state.users);
-  if (users === undefined && houses === undefined && reserves === undefined) {
-    return (<h4>Loading...</h4>);
-  }
-  return (
-    <div className="container">
-      <div className="row d-flex p-5">
-        <div className="table-responsive">
-          <table className="table table-striped table-hover">
-            <thead>
-              <tr>
-                <th scope="col">Date</th>
-                <th scope="col">Time</th>
-                <th scope="col">No. of Visitors</th>
-                <th scope="col">Visitor Name</th>
-                <th scope="col">House Address</th>
-              </tr>
-            </thead>
-            <tbody>
-              {reserves.map((reserve) => (
-                <tr key={reserve.id}>
-                  <th scope="row">
-                    {reserve.date}
-                  </th>
-                  <td>
-                    {new Date(reserve.time).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
-                  </td>
-                  <td>
-                    {reserve.visitors}
-                  </td>
-                  <td>
-                    {users.find((u) => u.id === reserve.user_id).name}
-                  </td>
-                  <td>
-                    {houses.find((h) => h.id === reserve.house_id).address}
-                  </td>
+
+  if (users !== undefined || houses !== undefined || reserves !== undefined) {
+    return (
+      <div className="container">
+        <div className="row d-flex p-5">
+          <div className="table-responsive">
+            <table className="table table-striped table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Date</th>
+                  <th scope="col">Time</th>
+                  <th scope="col">No. of Visitors</th>
+                  <th scope="col">Visitor Name</th>
+                  <th scope="col">House Address</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {reserves.map((reserve) => (
+                  <tr key={reserve.id}>
+                    <th scope="row">
+                      {reserve.date}
+                    </th>
+                    <td>
+                      {new Date(reserve.time).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+                    </td>
+                    <td>
+                      {reserve.visitors}
+                    </td>
+                    <td>
+                      {users.find((u) => u.id === reserve.user_id).name}
+                    </td>
+                    <td>
+                      {houses.find((h) => h.id === reserve.house_id).address}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
+    );
+  }
+  return (
+    dispatch(fetchReserves())
   );
 };
 
